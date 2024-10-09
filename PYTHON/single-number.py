61cddf0015cd6f0a03b8d732f8d5f7b7d8e2cdf0
SAIKAT KUMAR SARDAR <153124279+saikatsardar01@users.noreply.github.com>
2024-10-09 18:13:40 +0530

    Create single-number.py

---
+++ PYTHON/single-number.py
@@ -0,0 +1,6 @@
+# https://leetcode.com/problems/single-number/description/
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for i in nums:
+            if nums.count(i) == 1:
+                return(i)
